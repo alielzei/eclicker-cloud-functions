35b60782d4df7165476e2d6256194218bff0ce3f
Ali ElZein <alielzein753@gmail.com>
2020-03-25 19:47:49 +0200

    added createSession

--- functions/index.js
+++ functions/index.js
@@ -14,24 +14,38 @@ const db = admin.firestore();
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.helloWorld = functions.https.onRequest((req, res) => {
- req.send("Hello from Firebase!");
+    res.send("Hello from Firebase!");
 });
 
 exports.createSession = functions.https.onRequest((req, res) => {
-    // the template
-    db.collection('sessions').doc('5').set({
-        title: 'test placeholder',
-        options: ['opt1', 'opt2', 'opt3'],
+
+    console.log(req.body);
+
+    // res.send(JSON.stringify(req.body));
+
+    var title = req.body['title'] || "untitled";
+    var options = req.body['options'] || [];
+    var correct = req.body['correct'] || [];
+
+    ID = '6';
+
+    db.collection('sessions').doc(ID).set({
+        title: title,
+        options: options,
         // indexes of the correct answers in options
-        correct: [0, 1],
+        correct: correct,
         timeInSecs: 0,
         // the submissions of each option
         results: [ 0, 0, 0 ] 
+    }).then((result) => {
+        res.send(`success ${JSON.stringify(result)}`);
+    }).catch((error) => {
+        res.send(`err: ${JSON.stringify(error)}`);
     })
 
-    res.send('done')
 });
 
+// READ ALL SESSIONS FROM FIRESTORE
 // db.collection('sessions').get()
     // .then((snapshot) => {
     //   snapshot.forEach((doc) => {
