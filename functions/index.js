8ef8b91847d1a3cf8d5089c0ad6a67183e921929
Hussein <husseinelghoul@yahoo.com>
2020-03-26 22:37:06 +0200

    Added getSession :sparkles:

--- functions/index.js
+++ functions/index.js
@@ -11,24 +11,75 @@ const db = admin.firestore();
 exports.helloWorld = functions.https.onRequest((req, res) => {
     res.send("Hello from Firebase!");
 });
-
-exports.createSession = functions.https.onRequest((req, res) => {
-
-    var title = req.body['title'] || "untitled";
-    var options = req.body['options'] || [];
-    var correct = req.body['correct'] || [];
-    var time = req.body['timeInSecs'] || 0;
-    var results = req.body['results'] || [0,0,0,0];
-    ID = '8';
-    db.collection('sessions').doc(ID).set({
-        title: title,
-        options: options,
-        correct: correct,
-        timeInSecs: time,
-        results: [ 0, 0, 0 ] 
+//function that creats a session
+exports.createSession = functions.https.onRequest(async(req, res) => {
+    //Initilizing some variables for better readability
+    var _title = req.body['title'] || "untitled";
+    var _options = req.body['options'] || [];
+    var _correct = req.body['correct'] || [];
+    var _timeInSecs = req.body['timeInSecs'] || 0;
+    //optionsNumber holds the number that represents how many options you have
+    var optionsNumber = _options.length;
+    //this array will hold the results, it's initilized to 0s
+    var _results = [];
+    for(var i = 0; i < optionsNumber; i++) {
+        _results.push(0);
+    }
+    //Here I am using try and catch to force createSession function to wait
+    //untill the data is fetched (here size of my collection) from my database before proceeding
+    try{
+        await db.collection('sessions').get().then(snap => {
+            size = snap.size // will return the collection size
+        });
+    }
+    catch(err){
+        console.log(err);
+    }
+    //Without putting try and catch the function will fail here cause it will not get the type of size variable
+    ID = size+1;
+    db.collection('sessions').doc(`${ID}`).set({
+        title: _title,
+        options: _options,
+        correct: _correct,
+        timeInSecs: _timeInSecs,
+        results: _results,
     }).then((result) => {
-        res.send(`success ${JSON.stringify(result)}`);
+        res.send(`Session Crated Succesfuly!\n Your session ID is ${ID}.`);
     }).catch((error) => {
         res.send(`err: ${JSON.stringify(error)}`);
     })
+
 });
+//Function that takes a JSON File containing the ID of a session and Retrieves it
+exports.getSession = functions.https.onRequest(async (req,res)=>{
+    try{
+        const sessionId = req.body['ID'];
+        const snapshot  = await db.collection('sessions').doc(`${sessionId}`).get();
+        const data = snapshot.data()
+        if(data){
+            res.send(data);
+        }
+        else{
+            res.send("Please Check your session ID");
+        }
+        
+    }
+    catch(err){
+        //Handle the error
+        console.log("Error while executing function",err);
+        res.send("Error while getting the quiz");
+    }
+})
+//Helper Function that increments submissions field in session 1 by 1
+//This is for testing only
+exports.updateSubmits = functions.https.onRequest((req, res) => {
+    try{
+        const storyRef = db.collection('sessions').doc('0');
+        storyRef.update({ submits: admin.firestore.FieldValue.increment(1) })
+        res.send("Your results were submited !");
+    }
+    catch(err){
+        console.log("Error executing update function",err)
+        res.send("Error executing function");
+    }
+    })
