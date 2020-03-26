96d5bedc6e1f4d89f860913b6dc65d33c3140f55
Hussein <husseinelghoul@yahoo.com>
2020-03-26 02:44:22 +0200

    Fixing create session :bug:

--- functions/index.js
+++ functions/index.js
@@ -11,21 +11,38 @@ const db = admin.firestore();
 exports.helloWorld = functions.https.onRequest((req, res) => {
     res.send("Hello from Firebase!");
 });
-
-exports.createSession = functions.https.onRequest((req, res) => {
-
-    var title = req.body['title'] || "untitled";
-    var options = req.body['options'] || [];
-    var correct = req.body['correct'] || [];
-    var time = req.body['timeInSecs'] || 0;
-    var results = req.body['results'] || [0,0,0,0];
-    ID = '8';
-    db.collection('sessions').doc(ID).set({
-        title: title,
-        options: options,
-        correct: correct,
-        timeInSecs: time,
-        results: [ 0, 0, 0 ] 
+//function that creats a session
+exports.createSession = functions.https.onRequest(async(req, res) => {
+    //Initilizing some variables for better readability
+    var _title = req.body['title'] || "untitled";
+    var _options = req.body['options'] || [];
+    var _correct = req.body['correct'] || [];
+    var _timeInSecs = req.body['timeInSecs'] || 0;
+    //optionsNumber holds the number that represents how many options you have
+    var optionsNumber = _options.length;
+    //this array will hold the results, it's initilized to 0s
+    var _results = [];
+    for(var i = 0; i < optionsNumber; i++) {
+        _results.push(0);
+    }
+    //Here I am using try and catch to force createSession function to wait
+    //untill the data is fetched (here size of my collection) from my database before proceeding
+    try{
+        await db.collection('sessions').get().then(snap => {
+            size = snap.size // will return the collection size
+        });
+    }
+    catch(err){
+        console.log(err);
+    }
+    //Without putting try and catch the function will fail here cause it will not get the type of size variable
+    ID = size+1;
+    db.collection('sessions').doc(`${ID}`).set({
+        title: _title,
+        options: _options,
+        correct: _correct,
+        timeInSecs: _timeInSecs,
+        results: _results,
     }).then((result) => {
         res.send(`success ${JSON.stringify(result)}`);
     }).catch((error) => {
