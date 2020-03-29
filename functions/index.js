2a9f6048743d47410be6579832cbbbee26d89ba5
Hussein <husseinelghoul@yahoo.com>
2020-03-29 21:06:27 +0300

    Added update submits

--- functions/index.js
+++ functions/index.js
@@ -14,10 +14,10 @@ exports.helloWorld = functions.https.onRequest((req, res) => {
 //function that creats a session
 exports.createSession = functions.https.onRequest(async(req, res) => {
     //Initilizing some variables for better readability
-    var _title = req.body['title'] || "untitled";
-    var _options = req.body['options'] || [];
-    var _correct = req.body['correct'] || [];
-    var _timeInSecs = req.body['timeInSecs'] || 0;
+    var _title = req.body['title'];
+    var _options = req.body['options'];
+    var _correct = req.body['correct'];
+    var _timeInSecs = req.body['timeInSecs'];
     try{
         if(!(req.body['title'] && req.body['options'] && req.body['correct'] && req.body['timeInSecs'] )){
             res.send("Request Failed! few parameters.");
@@ -47,13 +47,14 @@ exports.createSession = functions.https.onRequest(async(req, res) => {
     }
     //Without putting try and catch the function will fail here cause it will not get the type of size variable
     ID = size+1;
-    db.collection('sessions').doc(`${ID}`).set({
-        title: _title,
-        options: _options,
-        correct: _correct,
-        timeInSecs: _timeInSecs,
-        results: _results,
-    }).then((result) => {
+    data = {
+            title: _title,
+            options: _options,
+            correct: _correct,
+            timeInSecs: _timeInSecs,
+            results : _results,
+    }
+    db.collection('sessions').doc(`${ID}`).set(data).then((result) => {
         res.send(`Session Crated Succesfuly!\n Your session ID is ${ID}.`);
     }).catch((error) => {
         res.send(`err: ${JSON.stringify(error)}`);
@@ -80,12 +81,18 @@ exports.getSession = functions.https.onRequest(async (req,res)=>{
         res.send("Error while getting the quiz");
     }
 })
-//Helper Function that increments submissions field in session 1 by 1
-//This is for testing only
-exports.updateSubmits = functions.https.onRequest((req, res) => {
+
+//This function takes a JSON file containing the ID of the session you wish to submit answers for
+//and the option name, kindly check example of a correct JSON file below
+// {	"ID" : 0,
+// 	"optionName" : "option1"
+// }
+exports.updateResults = functions.https.onRequest((req, res) => {
+    var _SessionID = req.body['ID']
+    var _optionName = req.body['optionName'];
     try{
-        const storyRef = db.collection('sessions').doc('0');
-        storyRef.update({ submits: admin.firestore.FieldValue.increment(1) })
+        const sessionRef = db.collection('sessions').doc(`${_SessionID}`);
+        sessionRef.update( `${_optionName}`,require('firebase-admin').firestore.FieldValue.increment(1));
         res.send("Your results were submited !");
     }
     catch(err){
@@ -93,3 +100,47 @@ exports.updateSubmits = functions.https.onRequest((req, res) => {
         res.send("Error executing function");
     }
     })
+
+//I'm trying to create fields to hold the results, for each option one field
+exports.testCreateSession = functions.https.onRequest(async(req, res) => {
+    //Initilizing some variables for better readability
+    var _title = req.body['title'] || "How many days is there in a week ?";
+    var _options = req.body['options'] || ["3","5","7"];
+    var _correct = req.body['correct'] || [1];
+    var _timeInSecs = req.body['timeInSecs'] || 30;
+    //optionsNumber holds the number that represents how many options you have
+    var optionsNumber = _options.length;
+    //this array will hold the results, it's initilized to 0s
+    var _results = [];
+    for(var i = 0; i < optionsNumber; i++) {
+        _results.push(0);
+    }
+    //Here I am using try and catch to force createSession function to wait
+    //untill the data is fetched (here size of my collection) from my database before proceeding
+    try{
+        await db.collection('sessions').get().then(snap => {
+            size = snap.size // will return the collection size
+        });
+    }
+    catch(err){
+        console.log(err);
+    }
+    ID = size+1;
+    sessionRef = db.collection('sessions').doc(`${ID}`);
+    data = {
+            title: _title,
+            options: _options,
+            correct: _correct,
+            "timeInSecs": _timeInSecs,
+    }
+    for(var i = 0;i<optionsNumber;i++){
+        var name = "option1"
+        sessionRef.set({"option1" : 0},{merge:true})
+    }
+    sessionRef.set(data).then((result) => {
+        res.send(`Session Crated Succesfuly!\n Your session ID is ${ID}.`);
+    }).catch((error) => {
+        res.send(`err: ${JSON.stringify(error)}`);
+    })
+
+});
