b11be6a0e624cb54d6357923a6685b4ab0c9bff4
Ali ElZein <alielzein753@gmail.com>
2020-03-29 23:53:21 +0300

    sprint3 by hussein

--- functions/index.js
+++ functions/index.js
@@ -1,30 +1,31 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 var serviceAccount = require("./serviceAccountKey.json");
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://eclicker-1.firebaseio.com"
 });
+
 const db = admin.firestore();
 
-//Start writing functions
-exports.helloWorld = functions.https.onRequest((req, res) => {
-    res.send("Hello from Firebase!");
-});
-//function that creats a session
+//function that creates a session
 exports.createSession = functions.https.onRequest(async(req, res) => {
-    //Initilizing some variables for better readability
+    //Initializing some variables for better readability
     var _title = req.body['title'] || "untitled";
     var _options = req.body['options'] || [];
     var _correct = req.body['correct'] || [];
     var _timeInSecs = req.body['timeInSecs'] || 0;
+
     //optionsNumber holds the number that represents how many options you have
     var optionsNumber = _options.length;
+
     //this array will hold the results, it's initilized to 0s
     var _results = [];
     for(var i = 0; i < optionsNumber; i++) {
         _results.push(0);
     }
+
     //Here I am using try and catch to force createSession function to wait
     //untill the data is fetched (here size of my collection) from my database before proceeding
     try{
@@ -35,6 +36,7 @@ exports.createSession = functions.https.onRequest(async(req, res) => {
     catch(err){
         console.log(err);
     }
+
     //Without putting try and catch the function will fail here cause it will not get the type of size variable
     ID = size+1;
     db.collection('sessions').doc(`${ID}`).set({
@@ -50,10 +52,11 @@ exports.createSession = functions.https.onRequest(async(req, res) => {
     })
 
 });
+
 //Function that takes a JSON File containing the ID of a session and Retrieves it
 exports.getSession = functions.https.onRequest(async (req,res)=>{
     try{
-        const sessionId = req.body['ID'];
+        const sessionId = req.query['ID'];
         const snapshot  = await db.collection('sessions').doc(`${sessionId}`).get();
         const data = snapshot.data()
         if(data){
@@ -62,7 +65,6 @@ exports.getSession = functions.https.onRequest(async (req,res)=>{
         else{
             res.send("Please Check your session ID");
         }
-        
     }
     catch(err){
         //Handle the error
@@ -70,6 +72,7 @@ exports.getSession = functions.https.onRequest(async (req,res)=>{
         res.send("Error while getting the quiz");
     }
 })
+
 //Helper Function that increments submissions field in session 1 by 1
 //This is for testing only
 exports.updateSubmits = functions.https.onRequest((req, res) => {
--- index.js
+++ index.js
@@ -29,11 +29,6 @@ exports.createSession = functions.https.onRequest(async(req, res) => {
     }
   //optionsNumber holds the number that represents how many options you have
     var optionsNumber = _options.length;
-    //this array will hold the results, it's initilized to 0s
-    var _results = [];
-    for(var i = 0; i < optionsNumber; i++) {
-        _results.push(0);
-    }
     //Here I am using try and catch to force createSession function to wait
     //untill the data is fetched (here size of my collection) from my database before proceeding
     try{
@@ -51,7 +46,6 @@ exports.createSession = functions.https.onRequest(async(req, res) => {
             options: _options,
             correct: _correct,
             timeInSecs: _timeInSecs,
-            results : _results,
     }
     db.collection('sessions').doc(`${ID}`).set(data).then((result) => {
         res.send(`Session Crated Succesfuly!\n Your session ID is ${ID}.`);
@@ -99,47 +93,3 @@ exports.updateResults = functions.https.onRequest((req, res) => {
         res.send("Error executing function");
     }
     })
-
-//I'm trying to create fields to hold the results, for each option
-exports.testCreateSession = functions.https.onRequest(async(req, res) => {
-    //Initilizing some variables for better readability
-    var _title = req.body['title'] || "How many days is there in a week ?";
-    var _options = req.body['options'] || ["3","5","7"];
-    var _correct = req.body['correct'] || [1];
-    var _timeInSecs = req.body['timeInSecs'] || 30;
-    //optionsNumber holds the number that represents how many options you have
-    var optionsNumber = _options.length;
-    //this array will hold the results, it's initilized to 0s
-    var _results = [];
-    for(var i = 0; i < optionsNumber; i++) {
-        _results.push(0);
-    }
-    //Here I am using try and catch to force createSession function to wait
-    //untill the data is fetched (here size of my collection) from my database before proceeding
-    try{
-        await db.collection('sessions').get().then(snap => {
-            size = snap.size // will return the collection size
-        });
-    }
-    catch(err){
-        console.log(err);
-    }
-    ID = size+1;
-    sessionRef = db.collection('sessions').doc(`${ID}`);
-    data = {
-            title: _title,
-            options: _options,
-            correct: _correct,
-            "timeInSecs": _timeInSecs,
-    }
-    for(var i = 0;i<optionsNumber;i++){
-        var name = "option1"
-        sessionRef.set({"option1" : 0},{merge:true})
-    }
-    sessionRef.set(data).then((result) => {
-        res.send(`Session Crated Succesfuly!\n Your session ID is ${ID}.`);
-    }).catch((error) => {
-        res.send(`err: ${JSON.stringify(error)}`);
-    })
-
-});
